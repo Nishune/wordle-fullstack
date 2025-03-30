0e36e1cf9c285c0e26939eb5b0da3cc6638a3066
Rikard Engström <rikardengstrom89@gmail.com>
2025-03-30 15:47:30 +0200

    refractor some code in backend, to make the app.js file more clean. Now got the different API codes in their own files.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import path from "path";
-import selectWord from "./utils/selectWord.js";
-import { getWordList } from "./utils/loadWordList.js";
+import handleGetWord from "./utils/getRandomWord.js";
 
 const app = express();
 
+const activeGames = new Map();
 /////
 // Middleware
 /////
@@ -16,36 +16,18 @@ app.use(express.static("../client/dist"));
 /////
 // API Routes
 /////
-app.get("/api/word", (req, res) => {
-  const wordList = getWordList();
-  // Kontrollera att ordlistan är laddad
-  if (!wordList.length) {
-    return res.status(500).json({ error: "Wordlist is not loaded yet." });
-  }
 
-  // Hämtar parametrar från query string
-  const length = parseInt(req.query.length) || 5; // Default längd: 5
-  const uniqueLetters = req.query.unique === "true"; // Default: false
+//API for retrieving a word.
+app.get("/api/word", handleGetWord);
 
-  try {
-    const word = selectWord(wordList, length, uniqueLetters);
-    if (!word) {
-      return res.status(404).json({
-        error: "No words matched the criteria",
-        params: { length, uniqueLetters },
-      });
-    }
-    res.json({ word });
-  } catch (error) {
-    console.error("Error when choosing word:", error);
-    res.status(500).json({ error: "Server error when choosing word." });
-  }
-});
+///
+//API
+///
 
 /////
 // Client Routes
 /////
-// Hantera alla andra routes genom att skicka index.html
+
 app.get("*", (req, res) => {
   res.sendFile(path.resolve("../client/dist/index.html"));
 });
