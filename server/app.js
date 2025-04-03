d625d61312848d565bb63efdb0023714a53f1774
Rikard Engström <rikardengstrom89@gmail.com>
2025-04-03 14:15:40 +0200

    Got the SSR page to function with almost similar styling as with MUI on the frontend. Started working on the highscore functionality.

--- server/app.js
+++ server/app.js
@@ -6,12 +6,21 @@ import handleGameGuess from "./utils/gameGuess.js";
 const app = express();
 
 /////
+//EJS Configuration
+/////
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+/////
 // Middleware
 /////
 app.use(express.json());
 
-// Servera statiska filer från dist-mappen
+// Serving static files from the dist folder React frontend.
 app.use(express.static("../client/dist"));
+
+//Serving static files for public folder backend.
+app.use("/static", express.static("./public"));
 
 /////
 // API Routes
@@ -19,6 +28,24 @@ app.use(express.static("../client/dist"));
 
 app.get("/api/game/new", handleNewGame);
 app.post("/api/game/:gameId/guess", handleGameGuess);
+
+/////
+//Server-Side Rendered Routes
+/////
+
+app.get("/highscore", (req, res) => {
+  try {
+    const highscores = [];
+
+    res.render("highscore", {
+      title: "Wordle highscore",
+      highscores: highscores,
+    });
+  } catch (error) {
+    console.log("Error loading highscores", error);
+    res.status(500).send("An error occurred while trying to get the highscore");
+  }
+});
 
 /////
 // Client Routes
