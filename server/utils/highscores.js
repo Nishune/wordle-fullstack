490fdd6711779749f2bcadebf2993b3e531a39d2
Rikard Engström <rikardengstrom89@gmail.com>
2025-04-04 15:21:51 +0200

    Added better structure and code comments in backend.

--- server/utils/highscores.js
+++ server/utils/highscores.js
@@ -15,23 +15,3 @@ export function addHighscore(scoreData) {
 export function getHighscores() {
   return highscores;
 }
-
-addHighscore({
-  name: "Example User",
-  time: 62000, // 1:02
-  guesses: [
-    {
-      guess: "HELLO",
-      feedback: [
-        { letter: "H", result: "correct" },
-        { letter: "E", result: "correct" },
-        { letter: "L", result: "correct" },
-        { letter: "L", result: "correct" },
-        { letter: "O", result: "correct" },
-      ],
-    },
-  ],
-  wordLength: 5,
-  uniqueLetters: false,
-  date: new Date(),
-});
