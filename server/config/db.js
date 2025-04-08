1eead608af66a80fad88462582e2d6e25b401c23
Rikard Engström <rikardengstrom89@gmail.com>
2025-04-08 09:53:53 +0200

    Just a small change to variable name from conn to connect to make it more readable.

--- server/config/db.js
+++ server/config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB Connected: ${connect.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
