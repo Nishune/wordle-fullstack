0a9072b32c5c5886e7c7824231e092cec08e05e5
Rikard Engström <rikardengstrom89@gmail.com>
2025-04-11 12:19:14 +0200

    Added db.ts, not sure if this one is good. Might have to check this one later.

--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -2,13 +2,15 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${connect.connection.host}`);
-    }
-    catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+    {
+        try {
+            const connect = await mongoose.connect(process.env.MONGODB_URI);
+            console.log(`MongoDB Connected: ${connect.connection.host}`);
+        }
+        catch (error) {
+            console.error(`error: ${error.message}`);
+            process.exit(1);
+        }
     }
 };
 export default connectDB;
