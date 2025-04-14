a2f828e80c7f77db82f14064ed6efadf06bac809
Rikard Engström <rikardengstrom89@gmail.com>
2025-04-14 13:34:10 +0200

    fixing some comments.

--- server/controllers/highscoreController.ts
+++ server/controllers/highscoreController.ts
@@ -6,27 +6,33 @@ export async function handleHighscores(
   res: Response
 ): Promise<void> {
   try {
+    // Check if wordlength is in the querystring
+    //if it is, convert to whole number, else set value to null.
     const wordLengthFilter = req.query.wordLength
       ? parseInt(req.query.wordLength as string)
       : null;
 
-    let uniqueLettersFilter;
+    let uniqueLettersFilter; //Stores the filtering of unique letters
+    // if uniqueLetters in the query is true, set filter to true
     if (req.query.uniqueLetters === "true") {
       uniqueLettersFilter = true;
+
+      // if its false, set filter to false
     } else if (req.query.uniqueLetters === "false") {
       uniqueLettersFilter = false;
     }
-
+    //Calling getfilt func, with filtered parameters
+    //Await the batabase query to complete.
     const highscores = await getFilteredHighscores(
       wordLengthFilter,
       uniqueLettersFilter
     );
 
     res.render("highscore", {
-      title: "Wordle Highscore",
-      highscores: highscores,
-      wordLengthFilter,
-      uniqueLettersFilter,
+      title: "Wordle Highscore", //Title highscore page
+      highscores: highscores, // The list with highscore from mongodb
+      wordLengthFilter, //the active filter for word length used to show active filter in ui
+      uniqueLettersFilter, // Same as word length, but for unique letters.
     });
   } catch (error) {
     console.log("Error loading highscores", error);
